638e58118255466e93c27169b32536f93c197693
@vcastro-dev <vcastro2395@gmail.com>
2023-11-24 23:46:48 -0300

    Criando form de "Sign in"

---
+++ app/signin/page.tsx
@@ -0,0 +1,17 @@
+import { Grid } from "@mui/material";
+import { SignInForm } from "../components/SigninForm";
+
+export default function Page() {
+  return (
+    <Grid
+      container
+      alignItems={"center"}
+      justifyContent={"center"}
+      height={"100%"}
+    >
+      <Grid item>
+        <SignInForm />
+      </Grid>
+    </Grid>
+  );
+}
