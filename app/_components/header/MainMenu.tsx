56bcdedc9bd6a823387cd901248d2ce95bc21f06
@vcastro-dev <vcastro2395@gmail.com>
2023-12-29 20:20:39 -0300

    fix: corrigindo erro de hidratação do Header

--- app/_components/header/MainMenu.tsx
+++ app/_components/header/MainMenu.tsx
@@ -4,60 +4,58 @@ import Link from "next/link";
 export function MainMenu() {
   return (
     <Toolbar>
-      <Box>
-        <Link href={"/"} passHref>
-          <Button
-            variant="text"
-            sx={{
-              color: "#fff",
-            }}
-          >
-            Home
-          </Button>
-        </Link>
+      <Link href={"/"} passHref>
+        <Button
+          variant="text"
+          sx={{
+            color: "#fff",
+          }}
+        >
+          Home
+        </Button>
+      </Link>
 
-        <Link href={"/leads"} passHref={true}>
-          <Button
-            variant="text"
-            sx={{
-              color: "#fff",
-            }}
-          >
-            Leads
-          </Button>
-        </Link>
+      <Link href={"/leads"} passHref={true}>
+        <Button
+          variant="text"
+          sx={{
+            color: "#fff",
+          }}
+        >
+          Leads
+        </Button>
+      </Link>
 
-        <Link href={"/clientes"} passHref={true}>
-          <Button
-            variant="text"
-            sx={{
-              color: "#fff",
-            }}
-          >
-            Clientes
-          </Button>
-        </Link>
-        <Link href={"/usuarios"} passHref={true}>
-          <Button
-            variant="text"
-            sx={{
-              color: "#fff",
-            }}
-          >
-            Usuários
-          </Button>
-          <Link href={"/calculadora"} passHref={true}>
-            <Button
-              variant="text"
-              sx={{
-                color: "#fff",
-              }}
-            >
-              Calculadora
-            </Button>
-          </Link>
-        </Link>
-      </Box>
+      <Link href={"/clientes"} passHref={true}>
+        <Button
+          variant="text"
+          sx={{
+            color: "#fff",
+          }}
+        >
+          Clientes
+        </Button>
+      </Link>
+      <Link href={"/usuarios"} passHref={true}>
+        <Button
+          variant="text"
+          sx={{
+            color: "#fff",
+          }}
+        >
+          Usuários
+        </Button>
+      </Link>
+      <Link href={"/calculadora"} passHref={true}>
+        <Button
+          variant="text"
+          sx={{
+            color: "#fff",
+          }}
+        >
+          Calculadora
+        </Button>
+      </Link>
     </Toolbar>
   );
 }
