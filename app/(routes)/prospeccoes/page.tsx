29e998ffe9857ad4de07ff2e605cc88019d8fcb2
@vcastro-dev <vcastro2395@gmail.com>
2024-03-12 19:43:16 -0300

    chore: renomeando componente

--- app/(routes)/prospeccoes/page.tsx
+++ app/(routes)/prospeccoes/page.tsx
@@ -1,4 +1,4 @@
-import { ProspeccoesList } from "@/src/components/lists/prospeccoes/ProspeccoesList";
+import { ProspeccoesList } from "@/src/components/lists/ProspeccoesList/ProspeccoesList";
 import { LeadQueryProvider } from "@/src/components/queryProviders/LeadQueryProvider";
 import { fetchErrorHandler } from "@/src/lib/errors/fetchErrorHandler";
 import { Prospeccao } from "@/src/types/prospeccao/Prospeccao";
